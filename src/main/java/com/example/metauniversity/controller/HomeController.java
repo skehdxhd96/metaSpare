5210dcf64b5adeb1a160153b39bedc59b204e613
spammy7147 <spammy7147@gmail.com>
2021-09-23 11:43:58 +0900

    HelloController -> HomeController

--- src/main/java/com/example/metauniversity/controller/HomeController.java
+++ src/main/java/com/example/metauniversity/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
     @GetMapping("/info")
     public String hello() {
